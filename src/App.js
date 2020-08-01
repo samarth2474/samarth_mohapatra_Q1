64fea9bd946bdf7343187bb9404b5c3730652fee
Samarth Mohapatra <samarthmmohapatra@gmail.com>
2020-08-01 17:50:54 +0530

    first Changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import  Cards  from './cards/Cards'
 import './App.css';
+const e = React.createElement;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const AppNav = () => (
+   <nav class="navbar navbar-dark bg-dark">
+       <a class="navbar-brand" href="#">My Blog</a>
+       <a role="button" class="btn btn-outline-info navbar-btn" href="/login">Login</a>
+   </nav>
+);
+class App extends React.Component {
+  constructor(props) {
+      super(props);
+  }
+
+  render() {
+      return (
+          <div>
+              <AppNav />
+              <div class="card mt-4" Style="width: 100%;">
+                  <div class="card-body">
+                      Please login to see your posts.
+        </div>
+              </div>
+              <Cards />
+          </div>
+      );
+  }
 }
-
 export default App;
